d44fc3b665e7a7a3d303d33dcf5e29eddd150862
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-11-20 15:03:43 -0500

    header added

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+        src="https://c.tenor.com/i_K3zWsgcG8AAAAi/hacker-pepe.gif"
+        alt="meme"
+        className="header--image"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">Meme Lords</h4>
+    </header>
+  )
+}
